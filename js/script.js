1ff8d245c3ce0e264a23ac6f34a0342bae4870de
Luisito Canlas <luisitocanlas@gmail.com>
2024-04-03 16:20:04 -0700

    Optimized function getComputerChoice()

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 function getComputerChoice() {
 	const choices = ['Rock', 'Paper', 'Scissors'];
-	let randomIdx = Math.floor(Math.random() * choices.length);
-
-	return choices[randomIdx];
+	return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
