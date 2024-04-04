9dee478c82775d706c6e80312ae9805fd81e2709
Luisito Canlas <luisitocanlas@gmail.com>
2024-04-03 20:50:57 -0700

    Initial refactor to utilize gui

--- js/script.js
+++ js/script.js
@@ -1,70 +1,66 @@
-function getComputerChoice() {
-	const choices = ['rock', 'paper', 'scissors'];
-	return choices[Math.floor(Math.random() * choices.length)];
-}
-
-function playRound(playerSelection, computerSelection) {
-	const result = {
-		rock: {
-			rock: `It's a tie!`,
-			paper: `Player loses the round!`,
-			scissors: `Player wins the round!`,
-		},
-		paper: {
-			rock: `Player wins the round!`,
-			paper: `It's a tie!`,
-			scissors: `Player loses the round!`,
-		},
-		scissors: {
-			rock: `Player loses the round!`,
-			paper: `Player wins the round!`,
-			scissors: `It's a tie!`,
-		},
-	};
-
-	roundResult.textContent = result[playerSelection][computerSelection];
-}
-
-function playGame() {
-	let playerWins = 0;
-	let computerWins = 0;
-
-	console.log(`Player won ${playerWins} times.`);
-	console.log(`Computer won ${computerWins} times.`);
-
-	if (playerWins > computerWins) {
-		console.log(`Player has won the game!`);
-	} else if (playerWins < computerWins) {
-		console.log(`Computer has won the game!`);
-	} else {
-		console.log(`It's a tie game!`);
-	}
-}
-
 const btnContainer = document.querySelector('#btn-container');
 const resultContainer = document.querySelector('#result-container');
-let roundResult = document.createElement('p');
+let result = document.createElement('p');
 let score = document.createElement('p');
+let rounds = 0;
+let playerWins = 0;
+let computerWins = 0;
+
+result.textContent = 'Select a button to start.';
+resultContainer.appendChild(result);
+
+score.textContent = ``;
+resultContainer.appendChild(score);
+
+function playGame(playerSelection) {
+	console.log(`Player won ${playerWins} times.`);
+	console.log(`Computer won ${computerWins} times.`);
+	score.textContent = `Player won ${playerWins} times.\n
+  Computer won ${computerWins} times.`;
+}
+
+function resetGame() {
+	if (playerWins > computerWins) {
+		console.log(`Player has won the game!`);
+		result.textContent = `Player has won the game!`;
+	} else if (playerWins < computerWins) {
+		console.log(`Computer has won the game!`);
+		result.textContent = `Computer has won the game!`;
+	} else {
+		console.log(`It's a tie game!`);
+		result.textContent = `It's a tie game!`;
+	}
+
+	playerWins = 0;
+	computerWins = 0;
+	rounds = 0;
+}
 
 btnContainer.addEventListener('click', (e) => {
 	let playerSelection = '';
 	switch (e.target.id) {
 		case 'btn-rock':
-			console.log('rock');
 			playerSelection = 'rock';
 			break;
 
 		case 'btn-paper':
-			console.log('paper');
 			playerSelection = 'paper';
 			break;
 
 		case 'btn-scissors':
-			console.log('scissors');
 			playerSelection = 'scissors';
 			break;
 	}
 
-	playRound(playerSelection, getComputerChoice());
-	resultContainer.append(roundResult);
+	if (rounds < 5) {
+		playGame(playerSelection);
+		rounds++;
+	} else {
+		resetGame();
+	}
 });
+
+function getComputerChoice() {
+	const choices = ['rock', 'paper', 'scissors'];
+	return choices[Math.floor(Math.random() * choices.length)];
+}
